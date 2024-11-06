2fc3732914e2987d0c1dee86487ed0e92e2484c3
joaorossi15 <joaorossiborba@gmail.com>
2024-11-05 21:37:28 -0300

    rag

---
+++ rag/rag.py
@@ -0,0 +1,62 @@
+from langchain.document_loaders.pdf import PyPDFDirectoryLoader # Importing PDF loader from Langchain
+from langchain.text_splitter import RecursiveCharacterTextSplitter # Importing text splitter from Langchain
+from langchain.embeddings import OpenAIEmbeddings # Importing OpenAI embeddings from Langchain
+from langchain.schema import Document # Importing Document schema from Langchain
+from langchain.vectorstores.chroma import Chroma # Importing Chroma vector store from Langchain
+from dotenv import load_dotenv # Importing dotenv to get API key from .env file
+from langchain.chat_models import ChatOpenAI # Import OpenAI LLM
+import os # Importing os module for operating system functionalities
+import shutil # Importing shutil module for high-level file operations
+
+
+def load_documents(dir: str):
+    doc_loader = PyPDFDirectoryLoader(dir)
+    return doc_loader.load()
+
+
+def split_text(docs: list[Document]):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=300,
+        chunk_overlap=100,
+        length_function=len,
+        add_start_index=True,
+    )
+
+    chunks = text_splitter.split_documents(docs)
+    print(f"Split {len(docs)} documents into {len(chunks)} chunks.")
+    
+    return chunks
+
+
+def chroma(chunks: list[Document]):
+    path = ''
+
+    if os.path.exists(path):
+        shutil.rmtree(path)
+
+    db = Chroma.from_documents(chunks, 
+                               # EMBEDDING, 
+                               persist_directory=path)
+    
+    db.persist()
+    print(f'Saved {len(chunks)} chunks to {path}')
+
+
+def generate_store(path: str):
+    documents = load_documents(path)
+    chunks = split_text(documents)
+    chroma(chunks)
+
+
+def query_rag(query: str):
+    # embedding function
+    # prepare database
+    # results of query
+    # check matching results
+    # combine context
+    # create template
+    # initialize model
+    # generate response
+    # get source of the matching docs
+    # format and return response
+    pass
